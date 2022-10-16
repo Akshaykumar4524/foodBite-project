9618b8c09188ea898322996e24a12c0d5eb8ee21
Akshaykumar4524 <patilakshay4524@gmail.com>
2022-10-16 22:28:16 +0530

    deploy on heroku

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -7,6 +7,7 @@ import foodRouter from './routers/food.router'
 import userRouter from "./routers/user.router";
 import orderRouter from "./routers/order.router";
 import { dbConnect } from './configs/database.config';
+import path from 'path'
 dbConnect();
 
 const app = express();
@@ -22,7 +23,12 @@ app.use("/api/foods",foodRouter)
 app.use("/api/users", userRouter)
 app.use("/api/orders", orderRouter)
 
-const port = 5000;
+app.use(express.static('public'));
+app.get('*',(req,res)=>{
+  res.sendFile(path.join(__dirname,'public','index.html'));
+})
+
+const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log("website served on http://localhost:" + port);
 });
